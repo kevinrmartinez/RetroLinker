08022507d203de47540087c751c00a43a897c698
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-08-28 10:32:20 -0400

    Algunos metodos de los botones en SettingsView creados

--- RetroarchShortcutterV2/Views/SettingsView.axaml.cs
+++ RetroarchShortcutterV2/Views/SettingsView.axaml.cs
@@ -18,14 +18,16 @@ namespace RetroarchShortcutterV2.Views
         private IClassicDesktopStyleApplicationLifetime deskWindow = (IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime;
 
         // PROPS/STATICS
-        private byte PrefTheme { get; set; }
-        private string PrefRADir { get; set; }
-        private string PrefROMPath { get; set; }
-        private bool PrevConfigs { get; set; }
-        private bool AllwaysDesktop { get; set; }
-        private bool CpyUserIcon { get; set; }
-        private string ConvIcoDir { get; set; }
-        private bool ExtractIco { get; set; }
+        private byte PrefTheme { get; set; } = Settings.PreferedTheme;
+        private string PrefRADir { get; set; } = Settings.DEFRADir;
+        private string PrefROMPath { get; set; } = Settings.DEFROMPath;
+        private bool PrevConfigs { get; set; } = Settings.PrevConfig;
+        private bool AllwaysDesktop { get; set; } = Settings.AllwaysDesktop;
+        private bool CpyUserIcon { get; set; } = Settings.CpyUserIcon;
+        private string ConvIcoDir { get; set; } = Settings.ConvICONPath;
+        private bool ExtractIco { get; set; } = Settings.ExtractIco;
+
+        private bool DesktopOS { get; } = System.OperatingSystem.IsWindows();
         
 
         // LOADS
@@ -37,8 +39,8 @@ namespace RetroarchShortcutterV2.Views
             Current_Window = windows_list[1];
 
             // Apariencia
-            Current_Window.RequestedThemeVariant = Settings.LoadThemeVariant();
-            switch(Settings.PreferedTheme)
+            //Current_Window.RequestedThemeVariant = Settings.LoadThemeVariant();
+            switch(PrefTheme)
             {
                 case 1: // Case de que el tema sea 'claro'
                     ThemeSwitch.IsChecked = false;
@@ -50,6 +52,22 @@ namespace RetroarchShortcutterV2.Views
                     ThemeDefault.IsChecked = true;
                     break;
             }
+
+            // OTROS
+            txtDefRADir.Text = PrefRADir;
+            txtDefROMPath.Text = PrefROMPath;
+            chkPrevCONFIG.IsChecked = PrevConfigs;
+            chkAllwaysDesktop.IsChecked = AllwaysDesktop;
+            chkCpyUserIcon.IsChecked = CpyUserIcon;
+            txtIcoSavPath.Text = System.IO.Path.GetFullPath(ConvIcoDir);
+            chkExtractIco.IsChecked = ExtractIco;
+            if (!DesktopOS)
+            {
+                panelWindowsOnlyControls.IsEnabled = false;
+                txtDefRADir.IsReadOnly = false;
+            }
+
+            
         }
 
         //APARIENCIA
@@ -89,7 +107,65 @@ namespace RetroarchShortcutterV2.Views
         }
 
         // EJECUTABLE
+        async void btnDefRADir_Click(object sender, RoutedEventArgs e)
+        {
+            int template;
+            if (DesktopOS) { template = 0; }        // FilePicker Option para .exe de Windows
+            else { template = 4; }                  // FilePicker Option para .AppImage de Windows
+            string file = await FileOps.OpenFileAsync(template, SettWindow);
+            if (file != null)
+            { txtDefRADir.Text = file; }
+        }
 
+        void btnsavDefRADir_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDefRADir.Text != string.Empty) { PrefRADir = txtDefRADir.Text; }
+        }
+
+        void btnclrDefRADir_Click(object sender, RoutedEventArgs e)
+        {
+            txtDefRADir.Text = string.Empty; PrefRADir = string.Empty;
+        }
+
+        // DIR PADRE
+        async void btnDefROMPath_Click(object sender, RoutedEventArgs e)
+        {
+            string file = await FileOps.OpenFileAsync(1, SettWindow);
+            if (file != null)
+            { txtDefROMPath.Text = file; }
+        }
+
+        void btnsavDefROMPath_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDefROMPath.Text != string.Empty) { PrefROMPath = txtDefROMPath.Text; }
+        }
+
+        void btnclrDefROMPath_Click(object sender, RoutedEventArgs e)
+        {
+            txtDefROMPath.Text = string.Empty; PrefROMPath = string.Empty;
+        }
+
+        // ICONS
+        // WINDOWS ONLY
+        async void btnIcoSavPath_Click(object sender, RoutedEventArgs e)
+        {
+            int template;
+            if (DesktopOS) { template = 3; }        // FilePicker Option para iconos de Windows
+            else { template = 5; }
+            string file = await FileOps.OpenFileAsync(template, SettWindow);
+            if (file != null)
+            { txtIcoSavPath.Text = file; }
+        }
+
+        void btnsavIcoSavPath_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtIcoSavPath.Text != string.Empty) { ConvIcoDir = txtIcoSavPath.Text; }
+        }
+
+        void btnclrIcoSavPath_Click(object sender, RoutedEventArgs e)
+        {
+            ConvIcoDir = FileOps.UserAssetsDir; txtIcoSavPath.Text = System.IO.Path.GetFullPath(ConvIcoDir);
+        }
 
         // Dialog Controls
         void btnDISSettings_Click(object sender, RoutedEventArgs e)
