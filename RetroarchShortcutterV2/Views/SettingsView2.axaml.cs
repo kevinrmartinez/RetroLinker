0066e24e7434a61f1f15c4db52761ea919ac7e6a
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-11-12 19:57:18 -0400

    Separacion de los views Settings en progreso. BROKEN COMMIT

--- RetroarchShortcutterV2/Views/SettingsView2.axaml.cs
+++ RetroarchShortcutterV2/Views/SettingsView2.axaml.cs
@@ -1,7 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using RetroarchShortcutterV2.Models;
+using RetroarchShortcutterV2.ViewModels;
 
 namespace RetroarchShortcutterV2.Views;
 
@@ -12,16 +14,18 @@ public partial class SettingsView2 : UserControl
         InitializeComponent();
     }
     
-    public SettingsView2(MainWindow mainWindow, bool desktopOs, Settings _settings)
+    public SettingsView2(MainWindow mainWindow, SettingsWindow settingsWindow, bool desktopOs, Settings _settings)
     {
         InitializeComponent();
-        ParentWindow = mainWindow;
+        MainAppWindow = mainWindow;
+        ParentWindow = settingsWindow;
         DesktopOS = desktopOs;
         settings = _settings;
     }
     
     // Window Obj
-    private MainWindow ParentWindow;
+    private MainWindow MainAppWindow;
+    private SettingsWindow ParentWindow;
     
     // PROPS/STATICS
     private bool FirstTimeLoad = true;
