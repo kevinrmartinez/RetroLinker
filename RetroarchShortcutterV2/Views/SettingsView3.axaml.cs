15f9af0d7ae7777a650673220c136a78a518d068
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-11-14 11:55:30 -0400

    Comienzo de SettingsView3

---
+++ RetroarchShortcutterV2/Views/SettingsView3.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RetroarchShortcutterV2.Views
+{
+    public partial class SettingsView3 : UserControl
+    {
+        public SettingsView3()
+        {
+            InitializeComponent();
+        }
+    }
+}
