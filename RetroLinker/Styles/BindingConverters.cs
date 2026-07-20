22115bbef9eae39f189c3c81b71f178c0c6cacc2
Kevin Martinez <kevinrmartinezj@gmail.com>
2026-07-20 19:32:09 -0400

    New binding converter added, used for showing UTC time into tocal time

--- RetroLinker/Styles/BindingConverters.cs
+++ RetroLinker/Styles/BindingConverters.cs
@@ -40,3 +40,32 @@ public class TextToLower : IValueConverter
         return value;
     }
 }
+
+public class DateToLocal : IValueConverter
+{
+    public static readonly DateToLocal Instance = new();
+    private DateTime ogDateTime; 
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTime dt)
+        {
+            ogDateTime = dt;
+            dt = dt.ToLocalTime();
+            if (targetType.IsAssignableTo(typeof(DateTime)) || targetType == typeof(object)) return dt;
+            else if (targetType.IsAssignableTo(typeof(string))) return dt.ToString(culture);
+        }
+        
+        // converter used for the wrong type
+        return new BindingNotification(new InvalidCastException($"targetType is '{targetType.Name}'"), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // throw new NotSupportedException();
+        // I think this is correct
+        if (targetType.IsAssignableTo(typeof(DateTime))) return ogDateTime;
+        else if (targetType.IsAssignableTo(typeof(string))) return ogDateTime.ToString(culture);
+        return value;
+    }
+}
