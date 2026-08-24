a50ba7738ab7f612b1d7718e250cee84ad4a3c14
Kevin Martinez <kevinrmartinezj@gmail.com>
2026-08-24 17:33:15 -0400

    Setting up `tileico`, part 1
    Now other settings toggles get disable when tileico is set

--- RetroLinker/Views/SettingsView3.axaml.cs
+++ RetroLinker/Views/SettingsView3.axaml.cs
@@ -292,8 +292,8 @@ namespace RetroLinker.Views
             {
                 LockControls(true);
                 var dialogTitle = resAvaloniaOps.dlgFileTileCliexe;
-                string currentFile = (string.IsNullOrEmpty(textBox.Text)) ? string.Empty : textBox.Text;
-                string file = await FileDialogOps.OpenFileAsync(OpenOpts.RAexe, ParentWindow, currentFile, dialogTitle);
+                var currentFile = (string.IsNullOrWhiteSpace(textBox.Text)) ? FileOps.BaseDir : textBox.Text;
+                var file = await FileDialogOps.OpenFileAsync(OpenOpts.RAexe, ParentWindow, currentFile, dialogTitle);
                 if (string.IsNullOrWhiteSpace(file)) return;
                 // txtUserAssets.Text = folder;
                 ParentWindow.NewSettings.TileIcoPath = file;
