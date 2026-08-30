657df4096dfaadd977abe993cebcb98c5b2a6057
Kevin Martinez <137180189+kevinrmartinez@users.noreply.github.com>
2026-08-30 01:23:24 -0400

    New wrapper read-only class for ShortcutManager(s)
    Reverted ShortcutterResult to a class.

--- RetroLinker/Models/Shortcutter.cs
+++ RetroLinker/Models/Shortcutter.cs
@@ -37,7 +37,7 @@ namespace RetroLinker.Models
         public string RApath
         {
             get => ra_path; 
-            private set { SetRApath(value); }   // TODO: Set later in the workflow (0.9)
+            private set { SetRApath(value); }   // TODO: Retire; move to LinkParameters (0.9)
         }      // 1
 
         public string ROMdir
@@ -49,12 +49,12 @@ namespace RetroLinker.Models
         public string ROMname
         {
             get => rom_name;
-            private set { SetROMname(value); }
+            private set { SetROMname(value); }  // TODO: Retire, it's not used in anything
         }    // 3
         public string ROMcore { get; set; }     // 4
         public string? CONFfile { get; set; }   // 5
-        public string? ICONfile { get; set; }   // 6
-        public string Command { get; set; }     // 7
+        public string ICONfile { get; set; }   // 6
+        public string Command { get; private set; }     // 7
         public string? Desc { get; set; }       // 8
         public List<ShortcutterOutput> OutputPaths { get; set; }    // 9
         public bool VerboseB { get; set; }      // 10
@@ -161,30 +161,28 @@ namespace RetroLinker.Models
             
             // Building the arguments
             Command = CommandManager.CommandBuilder(this);
-            return (os) ? BuildWinShortcut(winBuilderOpt) : BuildLinShorcut(this);
+            return (os) ? BuildWinShortcut(winBuilderOpt) : BuildLinShorcut();
         }
 
-        private ShortcutterResult CreateShortcut(ref readonly ShortcutterOutput output,
-            System.Action<Shortcutter, ShortcutterOutput> osCreateShortcut)
+        private ShortcutterResult CreateShortcut(ShortcutterOutput output,
+            System.Action<LinkParameters> osCreateShortcut)
         {
-            var result = new ShortcutterResult(output.FullPath);
+            var linkParams = new LinkParameters(this, output);
+            var result = new ShortcutterResult(linkParams.OutputPath);
             Logger.LogInfo($"Creating \"{result.OutputPath}\"...");
             try {
-                osCreateShortcut(this, output);
-                result.Message = result.Success1;
+                osCreateShortcut(linkParams);
+                result.ResultSuccess();
             }
-            catch (System.Exception ex)
-            {
+            catch (System.Exception ex) {
                 Logger.LogWarn($"\"{result.OutputPath}\" could not be created!");
                 Logger.LogErro(ex);
-                result.Message = result.Failure1;
-                result.Error = true;
-                result.ExMessage = ex.Message;
+                result.ResultFailure(ex.Message);
             }
             return result;
         }
         
-        // Windows
+        // == Windows ==
         private enum WindowsBuildOptions { Vbs, TileIco }
 
         private Dictionary<WindowsBuildOptions, System.Func<List<ShortcutterResult>>> _windowsBuilder = new();
@@ -197,9 +195,10 @@ namespace RetroLinker.Models
         private List<ShortcutterResult> BuildWinShortcut_Vbs()
         {
             var resultList = new List<ShortcutterResult>();
+            if (string.IsNullOrEmpty(ICONfile)) ICONfile = RAdir;
             Command = Utils.TwoDoubleQuotes(Command); // Add 2 double quotes for vbs compatibility
             foreach (var output in OutputPaths) {
-                var linkResult = CreateShortcut(in output, Windows.ShortcutCreator.CreateShortcut);
+                var linkResult = CreateShortcut(output, Windows.ShortcutManager.CreateShortcut);
                 resultList.Add(linkResult);
             }
             return resultList;
@@ -209,7 +208,7 @@ namespace RetroLinker.Models
         {
             var results = new List<ShortcutterResult>();
             var tileIcoOutput = OutputPaths.First();
-            var linkResult = CreateShortcut(in tileIcoOutput, Windows.ShortcutCreator.CreateTileIcoShortcut);
+            var linkResult = CreateShortcut(tileIcoOutput, Windows.ShortcutManager.CreateTileIcoShortcut);
             results.Add(linkResult);
             OutputPaths.Remove(tileIcoOutput);
             if (OutputPaths.Count > 0) results.AddRange(BuildWinShortcut_Vbs());
@@ -217,13 +216,13 @@ namespace RetroLinker.Models
         }
         
 
-        // Linux
-        private List<ShortcutterResult> BuildLinShorcut(Shortcutter link)
+        // == Linux ==
+        private List<ShortcutterResult> BuildLinShorcut()
         {
             var resultList = new List<ShortcutterResult>();
-            if (string.IsNullOrEmpty(link.ICONfile)) link.ICONfile = FileOps.DotDesktopRAIcon;
-            foreach (var output in link.OutputPaths) {
-                var linkResult = CreateShortcut(in output, Linux.ShortcutCreator.CreateShortcut);
+            if (string.IsNullOrEmpty(ICONfile)) ICONfile = FileOps.DotDesktopRAIcon;
+            foreach (var output in OutputPaths) {
+                var linkResult = CreateShortcut(output, Linux.ShortcutManager.CreateShortcut);
                 resultList.Add(linkResult);
             }
             return resultList;
@@ -235,7 +234,7 @@ namespace RetroLinker.Models
     public class ShortcutterOutput
     {
         public string FullPath { get; set; }
-        public string FriendlyName { get; set; }
+        public string FriendlyName { get; private set; }
         public string FileName { get; set; }
         public bool CustomEntryName { get; set; }
         public bool ValidOutput { get; private set; }
@@ -312,17 +311,60 @@ namespace RetroLinker.Models
             return (!DesktopOS && baseOutput.CustomEntryName) ? baseOutput : newOutput;
         }
     }
+
+
+    public class LinkParameters
+    {
+        public string RaExecutable { get; }
+        public string? RaWorkDir { get; }
+        public string RaArguments { get; }
+        public string? Description { get; }
+        public bool Verbose { get; }
+        public string IconPath { get; }
+        public string FriendlyName { get; }
+        public string OutputPath { get; }
+        public bool TileIcoNameVisible { get; }
+        public bool TileIcoNameDark { get; }
+        public bool TileIcoAllUsers { get; }
+        public string TileIcoImagePath { get; }
+
+        public LinkParameters(Shortcutter shortcut, ShortcutterOutput shortcutOutput)
+        {
+            RaExecutable = shortcut.RAdir;
+            RaWorkDir = FileOps.GetDirFromPath(RaExecutable);
+            RaArguments = shortcut.Command;
+            Description = shortcut.Desc;
+            Verbose = shortcut.VerboseB;
+            IconPath = shortcut.ICONfile;
+            FriendlyName = shortcutOutput.FriendlyName;
+            OutputPath = shortcutOutput.FullPath;
+            TileIcoNameVisible = shortcut.TileIcoNameVisible;
+            TileIcoNameDark = shortcut.TileIcoNameDark;
+            TileIcoAllUsers = shortcut.TileIcoAllUsers;
+            TileIcoImagePath = shortcut.TileIcoImage;
+        }
+    }
     
     
-    public struct ShortcutterResult(string outputPath)
+    public class ShortcutterResult(string outputPath)
     {
         public string OutputPath { get; } = outputPath;
-        public string? Message { get; set; }
-        public bool Error { get; set; }
-        public string? ExMessage { get; set; }
+        public string? Message { get; private set; }
+        public bool Error { get; private set; }
+        public string? ExMessage { get; private set; }
 
         
-        public readonly string Success1 = resMainView.popLinkSucces;
-        public readonly string Failure1 = resMainView.popLinkFailure;
+        private static readonly string Success1 = resMainView.popLinkSucces;
+        private static readonly string Failure1 = resMainView.popLinkFailure;
+
+        public void ResultSuccess() {
+            Message = Success1;
+        }
+
+        public void ResultFailure(string? exMessage = null) {
+            Message = Failure1;
+            Error = true;
+            ExMessage = exMessage;
+        }
     }
 }
